3ef722905b630431f18f87b25c34339233b65337
NatanBraslavsky <natanbraslavsky1@gmail.com>
2025-07-16 14:11:07 -0300

    função para remover do carrinho dinamico

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/cart.js'
+import {cart, removeFromCart} from '../data/cart.js'
 import {products} from '../data/products.js'
 import {formatCurrency} from './utils/money.js'
 
@@ -45,7 +45,7 @@ cart.forEach((cartItem)=>{
                             Update
                         </span>
                         <span
-                            class="delete-quantity-link link-primary"
+                            class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}"
                         >
                             Delete
                         </span>
@@ -110,3 +110,12 @@ cart.forEach((cartItem)=>{
 });
 
 document.querySelector('.order-summary').innerHTML = cartSummaryHTML;
+
+document.querySelectorAll('.js-delete-link')
+    .forEach((link)=>{
+        link.addEventListener('click', ()=>{
+            const productId = link.dataset.productId;
+            removeFromCart(productId);
+            console.log(cart)
+        })
+    })
