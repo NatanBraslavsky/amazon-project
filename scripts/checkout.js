a7cb6f7e1f29ed974b650196e92c12d7c3c22dd2
NatanBraslavsky <natanbraslavsky1@gmail.com>
2025-07-16 13:00:01 -0300

    formatação de preço no amazon.js e ajuste nos inputs com mesmo name

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -61,7 +61,7 @@ cart.forEach((cartItem)=>{
                             type="radio"
                             checked
                             class="delivery-option-input"
-                            name="delivery-option-1"
+                            name="delivery-option-${matchingProduct.id}"
                         />
                         <div>
                             <div class="delivery-option-date">
@@ -76,7 +76,7 @@ cart.forEach((cartItem)=>{
                         <input
                             type="radio"
                             class="delivery-option-input"
-                            name="delivery-option-1"
+                            name="delivery-option-${matchingProduct.id}"
                         />
                         <div>
                             <div class="delivery-option-date">
@@ -91,7 +91,7 @@ cart.forEach((cartItem)=>{
                         <input
                             type="radio"
                             class="delivery-option-input"
-                            name="delivery-option-1"
+                            name="delivery-option-${matchingProduct.id}"
                         />
                         <div>
                             <div class="delivery-option-date">
