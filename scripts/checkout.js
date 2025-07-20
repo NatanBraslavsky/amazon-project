f53fc64e13685bebd1812043018463da2df9736c
NatanBraslavsky <natanbraslavsky1@gmail.com>
2025-07-19 22:37:46 -0300

    data delivery dinamica

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,12 +1,9 @@
-import {cart, removeFromCart} from '../data/cart.js'
+import {cart, removeFromCart, updateDeliveryOption} from '../data/cart.js'
 import {products} from '../data/products.js'
 import {formatCurrency} from './utils/money.js'
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js'
-import {deliveryOptions, getDeliveryOption} from '../data/deliveryOptions.js'
+import {deliveryOptions} from '../data/deliveryOptions.js'
 
-const today = dayjs();
-const deliveryDate = today.add(7, 'days');
-console.log(deliveryDate.format('dddd, MMMM D'));
 
 let cartSummaryHTML = '';
 
@@ -22,9 +19,17 @@ cart.forEach((cartItem)=>{
         }
     })
 
+    
+
     const deliveryOptionId = cartItem.deliveryOptionId;
 
-    const deliveryOption = getDeliveryOption(deliveryOptionId);
+    let deliveryOption;
+
+    deliveryOptions.forEach((option) => {
+        if (option.id === deliveryOptionId) {
+            deliveryOption = option;
+        }
+    });
 
     const today = dayjs();
     const deliveryDate = today.add(
@@ -102,7 +107,7 @@ function deliveryOptionsHTML(matchingProduct, cartItem) {
         const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
 
         html += `
-            <div class="delivery-option">
+            <div class="delivery-option js-delivery-option" data-product-id="${matchingProduct.id}" data-delivery-option-id="${deliveryOption.id}}">
                 <input
                     type="radio"
                     ${isChecked ?'checked' : ''}
@@ -135,3 +140,11 @@ document.querySelectorAll('.js-delete-link')
             container.remove();
         })
     })
+
+document.querySelectorAll('.js-delivery-option')
+    .forEach((element)=>{
+        element.addEventListener('click', ()=>{
+            const {productId, deliveryOptionId} = element.dataset;
+            updateDeliveryOption(productId, deliveryOptionId);
+        });
+    });
